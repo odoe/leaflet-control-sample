b9d67a7a30ca1c27f1b8592cbb627ce9b5dac3f8
Rene Rubalcava <odoe@odoe.net>
2015-01-18 14:44:01 -0800

    update step2

--- js/controls/search.js
+++ js/controls/search.js
@@ -2,6 +2,19 @@
 // http://leafletjs.com/reference.html#icontrol
 // sample influenced by leaflet-geocoder
 // https://github.com/perliedman/leaflet-control-geocoder
+
+function sortParks(a, b) {
+  var _a = a.properties.park;
+  var _b = b.properties.park;
+  if (_a < _b) {
+    return -1;
+  }
+  if (_a > _b) {
+    return 1;
+  }
+  return 0;
+}
+
 L.Control.Search = L.Control.extend({
 	options: {
     // topright, topleft, bottomleft, bottomright
@@ -31,7 +44,32 @@ L.Control.Search = L.Control.extend({
     L.DomEvent.removeListener(this._input, 'keyup', this.keyup, this);
     L.DomEvent.removeListener(form, 'submit', this.find, this);
   },
-  keyup: function() {},
+  keyup: function(e) {
+    if (e.keyCode === 38 || e.keyCode === 40) {
+      this.keydown(e);
+    } else {
+      this.results.innerHTML = '';
+      if (this.input.value.length > 2) {
+        var value = this.input.value;
+        var results = _.take(_.filter(this.options.data, function(x) {
+          return x.properties.park.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        }).sort(sortParks), 10);
+        this.resultElems = _.map(results, function(x) {
+          var a = L.DomUtil.create('a', 'list-group-item');
+          a.href = '';
+          a.setAttribute('data-result-name', x.properties.park);
+          a.innerHTML = x.properties.park;
+          this.results.appendChild(a);
+          L.DomEvent.addListener(a, 'click', this.itemSelected, this);
+          return a;
+        }, this);
+        this._count = -1;
+      }
+    }
+  },
+  itemSelected: function(e) {
+    L.DomEvent.preventDefault(e);
+  },
   submit: function() {},
   find: function() {}
 });
