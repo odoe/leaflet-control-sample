6f842f0db8428796cecc49c36826ab4c7e86cc74
Rene Rubalcava <odoe@odoe.net>
2015-01-18 18:41:49 -0800

    updated search

--- js/controls/search.js
+++ js/controls/search.js
@@ -1,17 +1,17 @@
 // refer to the IControl interface
 // http://leafletjs.com/reference.html#icontrol
 L.Control.AutoComplete = L.Control.extend({
-	options: {
+  options: {
     // topright, topleft, bottomleft, bottomright
     position: 'topright'
-	},
-	initialize: function (options) {
-		// constructor
-	},
-	onAdd: function (map) {
-		// happens after added to map
-	},
-	onRemove: function (map) {
-		// when removed
-	}
+  },
+  initialize: function (options) {
+    // constructor
+  },
+  onAdd: function (map) {
+    // happens after added to map
+  },
+  onRemove: function (map) {
+    // when removed
+  }
 });
