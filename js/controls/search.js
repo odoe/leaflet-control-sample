b48161e6eba349c629e252057117b7f4181c61f2
Rene Rubalcava <odoe@odoe.net>
2015-01-18 23:26:21 -0800

    updated search

--- js/controls/search.js
+++ js/controls/search.js
@@ -50,11 +50,10 @@ L.Control.Search = L.Control.extend({
     } else {
       this.results.innerHTML = '';
       if (this.input.value.length > 2) {
-        var value = this.input.value;
         var results = _.take(_.filter(this.options.data, function(x) {
           return x.feature.properties.park.toUpperCase().indexOf(value.toUpperCase()) > -1;
         }).sort(sortParks), 10);
-        this.resultElems = _.map(results, function(x) {
+        _.map(results, function(x) {
           var a = L.DomUtil.create('a', 'list-group-item');
           a.href = '';
           a.setAttribute('data-result-name', x.feature.properties.park);
@@ -63,7 +62,6 @@ L.Control.Search = L.Control.extend({
           L.DomEvent.addListener(a, 'click', this.itemSelected, this);
           return a;
         }, this);
-        this._count = -1;
       }
     }
   },
